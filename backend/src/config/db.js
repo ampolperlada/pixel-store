a50984d3165d3894c6e75f09b9a3b701229ed5cd
Ampol Perlada <ampolperlada@gmail.com>
2025-03-24 21:30:00 +0800

    database #2

---
+++ backend/src/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+const prisma = new PrismaClient();
+
+export const connectPostgres = async () => {
+  try {
+    await prisma.$connect();
+    console.log("✅ PostgreSQL Connected!");
+  } catch (error) {
+    console.error("❌ PostgreSQL Connection Failed:", error);
+    process.exit(1);
+  }
+};
+
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("✅ MongoDB Connected!");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  }
+};
