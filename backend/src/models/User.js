724c00e578edefb2b65f89ca45f85a17f02ad694
Ampol Perlada <ampolperlada@gmail.com>
2025-03-30 20:38:45 +0800

    modified User Model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,21 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, required: true, unique: true },
-    password: String, // Hashed password
-    googleId: String, // For Google OAuth
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 }, { timestamps: true });
 
-export default mongoose.model('User', UserSchema);
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
