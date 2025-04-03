0a432a4c95b59de7e734ed5298af54a0310962a3
Ampol Perlada <ampolperlada@gmail.com>
2025-04-03 23:05:51 +0800

    npx prisma generate --schema=./backend/prisma/schema.prisma

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,11 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL
-    },
-  },
-});
-
-export default prisma;
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+export default prisma
