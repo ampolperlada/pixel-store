8fc2589737a221e0440873a406627535e87ab29a
Ampol Perlada <ampolperlada@gmail.com>
2025-04-06 23:57:39 +0800

    importing prototype trending sec

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,8 @@ import EducationHub from "./components/EducationHub";
 import ArtistShowcase from "./components/ArtistShowcase";
 import HowItWorks from "./components/HowItWorks";
 import RoyaltySystem from "./components/Royalty";
+import TrendingSection from './components/TrendingSection';
+
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
