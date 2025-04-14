e40d4fe20d6d907b52f9a5e0c0a9a8af72f6c8aa
Ampol Perlada <ampolperlada@gmail.com>
2025-04-14 23:15:40 +0800

    debugging routes signup and json

--- app/api/[...nextauth]/route.ts
+++ app/api/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 // /app/api/auth/[...nextauth]/route.ts
 
 import NextAuth from "next-auth"
-import authOptions from "./lib/auth" // Make sure this path is correct
+import { authOptions } from "../lib/auth" // Make sure this path is correct
 
 const handler = NextAuth(authOptions)
 
