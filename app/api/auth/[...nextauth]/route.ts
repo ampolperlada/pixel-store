4283344d5113295ad3b03d482a753af180f59e2a
Ampol Perlada <ampolperlada@gmail.com>
2025-04-17 14:49:14 +0800

    fixing routes misnamed and proper importing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth"
-import { authOptions } from "../lib/auth"
+import { authOptions } from "../../lib/auth"
 import { NextResponse } from "next/server"
 
 const handler = NextAuth(authOptions)
