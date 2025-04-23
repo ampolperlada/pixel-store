e562e9b972fc4db476dfc6ae2848718e0858bdd7
Ampol Perlada <ampolperlada@gmail.com>
2025-04-23 22:43:51 +0800

    Protecting routes (Explore/page.tsx)

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,12 @@
 // app/explore/page.tsx
+'use client'; // Add this since we're using client-side protection
 import ExploreArt from '../components/ExploreArt';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 export default function ExplorePage() {
-  return <ExploreArt />;
+  return (
+    <ProtectedRoute>
+      <ExploreArt />
+    </ProtectedRoute>
+  );
 }
